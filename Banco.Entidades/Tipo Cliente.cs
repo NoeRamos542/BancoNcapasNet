322b94b8a25863c3e62d3b984bb155e212ee8c1f
NoeRamos542 <ndeyvid147@gmail.com>
2023-02-24 00:14:55 -0500

    Se agrego las Entidades Cleinte, TipoCliente y TipoDocumento

---
+++ Banco.Entidades/Tipo Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace Banco.Entidades
+{
+    public class Tipo_Cliente
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string MyProperty { get; set; }
+        public bool Estado { get; set; }
+    }
+}
